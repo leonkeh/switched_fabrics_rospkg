e49d8f4aa1a804cb96a9e5c9efe6999d5e90a797
leon <Leon.Kehler@outlook.com>
2025-01-07 17:31:32 +0100

    added multi goal experiment client option

--- scripts/switched_joint_client.py
+++ scripts/switched_joint_client.py
@@ -13,17 +13,15 @@ from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Float32
 
 def default_goal(goal):
-    #default goal:
-    joint_xy = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    goal.goal_joints.data = joint_xy
-    goal.goal_threshold = 0.01
+    goal.goal_joints.data = rospy.get_param('joint_goal', [2.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    goal.goal_threshold = rospy.get_param('goal_threshold', 0.01)
     return goal
 
-def fabrics_client():
+def fabrics_client(schedule):
     if len(sys.argv)>1:
         robot_name = sys.argv[1]
     else:
-        print("please specify robot name as an argument, e.g.: dingo1, dingo2")
+        print("please specify robot name as an argument, e.g.: dingo1, dingo2, dinova (in the simulator)")
         return
     # Creates the SimpleActionClient, passing the type of the action
     client = actionlib.SimpleActionClient(robot_name+'/switched_action_server', dinova_fabrics_msgs.msg.FabricsJointAction)
@@ -33,20 +31,35 @@ def fabrics_client():
 
     # Creates a goal to send to the action server.
     goal = dinova_fabrics_msgs.msg.FabricsJointGoal(goal_joints=Float64MultiArray(), goal_threshold=Float32())
-    goal = default_goal(goal=goal)
+    
+    if schedule == "single_goal":
+        goal = default_goal(goal=goal)
 
-    # Sends the goal to the action server.
-    client.send_goal(goal)
+        # Sends the goal to the action server.
+        client.send_goal(goal)
 
-    # Waits for the server to finish performing the action.
-    max_time = 30.
-    status = client.wait_for_result(timeout=rospy.Duration(max_time))
-    if status == False:
-        client.cancel_goal()
-        print("ended after timeout")
-    else:
-        print("status:", status)
-        
+        # Waits for the server to finish performing the action.
+        max_time = 30.
+        status = client.wait_for_result(timeout=rospy.Duration(max_time))
+        if status == False:
+            client.cancel_goal()
+            print("ended after timeout")
+        else:
+            print("status:", status)
+
+    elif schedule == "experiment":
+        goals = [[1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+        for config_goal in goals:
+            rospy.set_param('joint_goal', config_goal)
+            goal = default_goal(goal=goal)
+            client.send_goal(goal)
+            max_time = 30.
+            status = client.wait_for_result(timeout=rospy.Duration(max_time))
+            if status == False:
+                client.cancel_goal()
+                print("ended after timeout")
+            else:
+                print("status:", status)
         
     # Prints out the result of executing the action
     return client.get_result()
@@ -56,6 +69,8 @@ if __name__ == '__main__':
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
         rospy.init_node('fabrics_client_py')
-        result = fabrics_client()
+        schedule = sys.argv[2] if len(sys.argv) > 2 else "single_goal"
+        result = fabrics_client(schedule)
+
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
